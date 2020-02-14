3f5db718e68ecd24c39c24be87c78f8ff5275179
HR <harleyroper96@gmail.com>
2020-02-13 21:43:57 -0600

    changed site locations

--- src/Engine/Horoscopes.java
+++ src/Engine/Horoscopes.java
@@ -10,7 +10,7 @@ import static org.apache.commons.lang3.text.WordUtils.wrap;
 
 public class Horoscopes {
     
-    String url = "https://www.ganeshaspeaks.com/horoscopes/daily-horoscope/";
+    String url = "https://www.horoscope.com/us/horoscopes/general/";
     
     Document doc = Jsoup.connect(url).get();
     
@@ -18,70 +18,70 @@ public class Horoscopes {
     }
     
     public void AriesHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "aries/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=1").get();
         parseScopeDescription(doc);
     }
     
     public void TaurusHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "taurus/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=2").get();
         parseScopeDescription(doc);
     }
     
     public void GeminiHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "gemini/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=3").get();
         parseScopeDescription(doc);
     }
     
     public void CancerHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "cancer/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=4").get();
         parseScopeDescription(doc);
     }
     
     public void LeoHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "leo/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=5").get();
         parseScopeDescription(doc);
     }
     
     public void VirgoHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "virgo/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=6").get();
         parseScopeDescription(doc);
     }
     
     public void LibraHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "libra/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=7").get();
         parseScopeDescription(doc);
     }
     
     public void ScorpioHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "scorpio/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=8").get();
         parseScopeDescription(doc);
     }
     
     public void SagittariusHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "sagittarius/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=9").get();
         parseScopeDescription(doc);
     }
     
     public void CapricornHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "capricorn/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=10").get();
         parseScopeDescription(doc);
     }
     
     public void AquariusHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "aquarius/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=11").get();
         parseScopeDescription(doc);
     }
     
     public void PiscesHoroscope() throws IOException {
-        doc = Jsoup.connect(url + "pisces/").get();
+        doc = Jsoup.connect(url + "horoscope-general-daily-today.aspx?sign=12").get();
         parseScopeDescription(doc);
     }
     
     //parses web page and finds the horoscope description, then displays the text
     private void parseScopeDescription(Document doc) throws IOException {
         //finds horoscope description
-        Element link = doc.select("p").get(2);
-    
+        Element link = doc.select("p").get(0);
+        
         System.out.println(wrap(link.text(), 75));
         Main.ScopeSelection();
     }
